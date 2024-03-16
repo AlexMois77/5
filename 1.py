3a06526f541c197e645b207142982f6ae97f814a
AlexMois77 <159935651+AlexMois77@users.noreply.github.com>
2024-03-16 18:20:45 +0100

    3

--- 1.py
+++ 1.py
@@ -1,26 +1,6 @@
-'''
-Завдання: Сортування файлів у папці.
-Копіювати файли із зазначеної папки та покласти в нову папку з розширенням цього файлу.
-'''
-
-import argparse
-from pathlib import Path
-from shutil import copyfile
-
-output_folder = Path('dist')
-
-def read_folder(path: Path) -> None:
-    for element in path.iterdir():
-        if element.is_dir():  #  перевіряємо чи елемент є папкою
-            read_folder(element)  # Рекурсивно викликаємо функцію з новим шляхом
-        else:  # Тоді це файл
-            copy_file(element)  # Функція для копіювання файлів
-
-
-def copy_file(file: Path) -> None:
-    ext = file.suffix  # Отримуємо розширення з файлів: .JPG, .PNG, .SVG, etc.
-    new_path = output_folder / ext  # Будуємо новий шлях
-    new_path.mkdir(parents=True, exist_ok=True)  # Створюємо папку за новим шляхом
-    copyfile(file, new_path / file.name)  # Копіюємо файл
-
-read_folder(Path('picture'))
+# Видалення елементів із словника
+first = {'a': 10, 'b': 20, 'c': 30, 'd': 40}
+del first['b']
+print(first)
+first.clear()
+print(first)
